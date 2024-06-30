74acc7601c7c9e873de03af451e8eed42e68b32a
wonchul1998 <shinwc1998@naver.com>
2024-06-30 14:42:12 +0900

    ..

---
+++ baekjoon_cpp/baekjoon_cpp/2143.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int T, n, m;
+int A[1001], B[1001];
+
+
+int main() {
+	cin >> T;
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> A[i];
+	cin >> m;
+	for (int i = 1; i <= m; i++) cin >> B[i];
+	return 0;
+}
